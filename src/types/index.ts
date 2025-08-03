b21d3e3f4eaddc6d3af43292cacdf32de2f0cad8
Jesus Yael Padrón Grimaldo <jesusyaelpadrongrimaldo@gmail.com>
2025-08-02 20:01:50 -0600

    Se sube la visualizacion de las notas

--- src/types/index.ts
+++ src/types/index.ts
@@ -28,6 +28,18 @@ export const userSchema = authSchema.pick({
 
 export type User = z.infer<typeof userSchema>
 
+/** Notes */
+const noteSchema = z.object({
+    _id: z.string(),
+    content: z.string(),
+    createdBy: userSchema,
+    task: z.string(),
+    createdAt: z.string()
+})
+
+export type Note = z.infer<typeof noteSchema>
+export type NoteFormData = Pick<Note, 'content'>
+
 /** Tasks */
 export const taskStatusSchema = z.enum(["pending", "inProgress", "onHold", "underReview", "completed"])
 export type TaskStatus = z.infer<typeof taskStatusSchema>
@@ -42,6 +54,9 @@ export const taskSchema = z.object({
         _id: z.string(),
         user: userSchema,
         status: taskStatusSchema
+    })),
+    notes: z.array(noteSchema.extend({
+        createdBy: userSchema
     })),
     createdAt: z.string(),
     updatedAt: z.string()
